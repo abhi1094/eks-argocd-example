d5f42db282bf675315d160753a9cb1e877614d72
abhi1094 <44882655+abhi1094@users.noreply.github.com>
2023-10-24 21:38:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello World!'))
+app.get('/hello', (req, res) => res.send('CI CD Deployment through argocd!'))
 app.listen(3000, () => console.log('Server ready'))
