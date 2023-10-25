0ceb02236365444cdf98cb42f0bf2b0c42b25e0a
abhi1094 <44882655+abhi1094@users.noreply.github.com>
2023-10-25 14:47:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Argo CD Demo'))
+app.get('/hello', (req, res) => {
+  const htmlContent = '<html><body><h1>Hello, ArgoCD</h1></body></html>'
+  res.send(htmlContent)
+})
 app.listen(3000, () => console.log('Server ready'))
