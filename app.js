6fb11925ac831893c50c71f22fcdf445b36b2da2
abhi1094 <44882655+abhi1094@users.noreply.github.com>
2023-10-25 14:51:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require('express')
 const app = express()
 
 app.get('/hello', (req, res) => {
-  const htmlContent = '<html><body><h1>Hello, ArgoCD</h1></body></html>'
+  const htmlContent = '
+                        <html>
+                          <body
+                          <center>
+                          <h1>Hello,ArgoCD</h1> <br>
+                          <br>
+                          <img src='https://raw.githubusercontent.com/Ajaypathak372/cloud-task2/master/task2.jpg' width=600 height=480>
+                          </center>
+                          </body>
+                        </html>
+                      '
   res.send(htmlContent)
 })
 app.listen(3000, () => console.log('Server ready'))
